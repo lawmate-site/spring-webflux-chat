5c33725e1e2f2b39f9fb07f8c0b94e2fdb70d894
hohyun0406 <hohyun0406@gmail.com>
2024-06-21 17:14:20 +0900

    first commit

---
+++ src/main/java/spring_flux_chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package spring_flux_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
